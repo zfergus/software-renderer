e7d314472b5440da5a5d58f9356deed2b4f7e833
Zachary Ferguson <zfergus@nyu.edu>
2021-09-07 19:21:03 -0400

    Fixed stb_image recipe

--- cmake/recipes/stb_image.cmake
+++ cmake/recipes/stb_image.cmake
@@ -7,9 +7,10 @@ message(STATUS "Third-party: creating target 'igl::stbimage'")
 include(eigen)
 include(libigl)
 
-if(NOT TARGET stb_image)
-  # Download this myself for the software renderer
-  igl_download_stb()
-  add_subdirectory(${LIBIGL_EXTERNAL}/stb stb_image)
-  add_library(igl::stbimage ALIAS igl_stb_image)
+# This may have been downloaded by libigl depending on the OpenGL options
+if(NOT TARGET igl_stb_image)
+    igl_download_stb()
+    add_subdirectory(${LIBIGL_EXTERNAL}/stb stb_image)
 endif()
+
+add_library(igl::stbimage ALIAS igl_stb_image)
